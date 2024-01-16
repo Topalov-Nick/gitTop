25437706be290cb9888306d5bfde98e88bdc8b94
Topalov-Nick <topalov.nick07@gmail.com>
2024-01-16 23:35:35 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+"use strict"
+
+let age = 20;
+let namee = "Никита";
+let single = false;
+console.log(typeof age);
+console.log(typeof namee);
+console.log(typeof single);
+
+console.log(`Меня зовут ${namee}, мне ${age} лет, моё семейное положение ${single}`);
+
+
